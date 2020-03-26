847b017360099358f26f8ade11fadf965e4908db
Santosh <santoshkumarm1996@gmail.com>
2020-03-26 19:33:09 +0530

    Section-5  styles (26/03/20)

--- src/App.js
+++ src/App.js
@@ -1,90 +1,124 @@
-import React, { Component } from 'react';
-import Person from './Person/Person'
-import './App.css';
-
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      person: [
-        { name: "srinu", age: 25 },
-        { name: "Ram", age: 30 },
-        { name: "aravind", age: 25 }
-      ],
-      isPersonDetailsEnabled: false
-    }
-
-  }
-  
-
-  togglePesonDetails = () => {
-    this.setState({
-      isPersonDetailsEnabled: !this.state.isPersonDetailsEnabled
-
-    })
-  }
-
-  nameChangHandler = (event) => {
-    this.setState({
-      person: [
-        { name: "srinu", age: 25 },
-        { name: event.target.value, age: 30 },
-        { name: "Balu", age: 25 }
-      ]
-
-    })
-  }
-
-  deletePersonDetail = (selectedndex) => {
-    let persons = this.state.person
-    persons.splice(selectedndex, 1)
-    this.setState({
-      person: persons
-    })
-
-  }
-
-  render() {
-    let personData = null
-    if (this.state.isPersonDetailsEnabled) {
-      personData = this.state.person.map((person, index) => {
-        return (
-          <Person name={person.name} age={person.age}
-            selectedindex={index}
-            deletePersonDetail={(index) => this.deletePersonDetail(index)} nameChangHandler={(e) => this.nameChangHandler(e)} />
-        )
-      })
-    }
-    return (
-      <div className="App">
-
-        <h1>I am a React App</h1>
-        <p>This is really working</p>
-        <button onClick={this.togglePesonDetails}>Switch</button>
-        {/* {
-          this.state.isPersonDetailsEnabled ?
-            <div>
-              <Person name={"santosh"} age="22"><b>Hobbies:Playing Cricket</b></Person>
-              <Person name="manohar" age="24" />
-
-            </div>
-            :
-            null
-        } */}
-        {personData}
-
-      </div>
-    );
-  }
-}
-
-export default App;
 
 
+//--using radium stryle component-----------
+// import React, { Component } from 'react';
+// import Person from './Person/Person'
+// import './App.css';
+// import Radium,{StyleRoot} from 'radium'
 
 
-// Using functional component and react life cycle hooks
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       person: [
+//         { id: 0, name: "srinu", age: 25 },
+//         { id: 1, name: "Ram", age: 30 },
+//         { id: 2, name: "aravind", age: 25 }
+//       ],
+//       isPersonDetailsEnabled: false
+//     }
+
+//   }
+
+
+//   togglePesonDetails = () => {
+//     this.setState({
+//       isPersonDetailsEnabled: !this.state.isPersonDetailsEnabled
+
+//     })
+//   }
+
+//   nameChangHandler = (event, id) => {
+//     const personIndex = this.state.person.findIndex((p) =>{return p.id === id})
+//     const person = { ...this.state.person[personIndex] }
+//     person.name = event.target.value;
+//     let persons = [...this.state.person];
+//     persons[personIndex] =Object.assign(persons[personIndex] ,person)
+//       this.setState({
+//         person: persons
+//       })
+//   }
+
+//   deletePersonDetail = (selectedndex) => {
+//     let persons = this.state.person
+//     persons.splice(selectedndex, 1)
+//     this.setState({
+//       person: persons
+//     })
+
+//   }
+
+//   render() {
+//     let personData = null
+//     const style={
+//       backgroundColor:"green",
+//       color:"white",
+//       fontSize:17,
+//       padding:16,
+//       cursor:"pointer",
+//       ':hover':{
+//         backgroundColor:'lightGreen',
+//         color:"blue"
+//       },
+//     }
+//     if (this.state.isPersonDetailsEnabled) {
+//       personData = this.state.person.map((person, index) => {
+//         return (
+//           <Person name={person.name} age={person.age}
+//             selectedindex={index}
+//             deletePersonDetail={(index) => this.deletePersonDetail(index)}
+//             nameChangHandler={(e) => this.nameChangHandler(e, person.id)}
+//             key={person.id}
+//           />
+//         )
+//       })
+//       style.backgroundColor="red"
+//       style[':hover']={
+//         backgroundColor:'samlon',
+//         color:"green"
+//       }
+//     }
+
+//     const classes=[];
+//     if(this.state.person.length<=1){
+//       classes.push("red")
+//     }
+//     else if(this.state.person.length >= 2){
+//       classes.push("bold")
+//     }
+//     return (
+//       <StyleRoot>
+//       <div className="App">
+
+//         <h1>I am a React App</h1>
+//         <p className={classes.join(" ")}> This is really working</p>
+//         <button style={style} onClick={this.togglePesonDetails}>Toggle Person</button>
+//         {/* {
+//           this.state.isPersonDetailsEnabled ?
+//             <div>
+//               <Person name={"santosh"} age="22"><b>Hobbies:Playing Cricket</b></Person>
+//               <Person name="manohar" age="24" />
+
+//             </div>
+//             :
+//             null
+//         } */}
+//         {personData}
+
+//       </div>
+//       </StyleRoot>
+//     );
+//   }
+// }
+
+// export default Radium(App);
+
+
+
+
+// -------Using functional component and react life cycle hooks---------
 
 // import React,{Component, useState} from 'react';
 // import logo from './logo.svg';
@@ -142,3 +176,130 @@ export default App;
 
 // export default App;
 
+
+
+import React, { Component } from 'react';
+import Person from './Person/Person'
+import './App.css';
+// import Radium,{StyleRoot} from 'radium'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+background-color:${props=>props.alt?'red':"green"},
+color:white,  
+font-size:17,
+padding:16,
+cursor:pointer,
+&:hover {
+  background-color:${props=>props.alt?"green":"lightGreen"},
+  color:blue
+`;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      person: [
+        { id: 0, name: "srinu", age: 25 },
+        { id: 1, name: "Ram", age: 30 },
+        { id: 2, name: "aravind", age: 25 }
+      ],
+      isPersonDetailsEnabled: false
+    }
+
+  }
+
+
+  togglePesonDetails = () => {
+    this.setState({
+      isPersonDetailsEnabled: !this.state.isPersonDetailsEnabled
+
+    })
+  }
+
+  nameChangHandler = (event, id) => {
+    const personIndex = this.state.person.findIndex((p) => { return p.id === id })
+    const person = { ...this.state.person[personIndex] }
+    person.name = event.target.value;
+    let persons = [...this.state.person];
+    persons[personIndex] = Object.assign(persons[personIndex], person)
+    this.setState({
+      person: persons
+    })
+  }
+
+  deletePersonDetail = (selectedndex) => {
+    let persons = this.state.person
+    persons.splice(selectedndex, 1)
+    this.setState({
+      person: persons
+    })
+
+  }
+
+  render() {
+    let personData = null
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      fontSize: 17,
+      padding: 16,
+      cursor: "pointer",
+      ':hover': {
+        backgroundColor: 'lightGreen',
+        color: "blue"
+      },
+    }
+    if (this.state.isPersonDetailsEnabled) {
+      personData = this.state.person.map((person, index) => {
+        return (
+          <Person name={person.name} age={person.age}
+            selectedindex={index}
+            deletePersonDetail={(index) => this.deletePersonDetail(index)}
+            nameChangHandler={(e) => this.nameChangHandler(e, person.id)}
+            key={person.id}
+          />
+        )
+      })
+      style.backgroundColor = "red"
+      style[':hover'] = {
+        backgroundColor: 'samlon',
+        color: "green"
+      }
+    }
+
+    const classes = [];
+    if (this.state.person.length <= 1) {
+      classes.push("red")
+    }
+    else if (this.state.person.length >= 2) {
+      classes.push("bold")
+    }
+    return (
+
+      <div className="App">
+
+        <h1>I am a React App</h1>
+        <p className={classes.join(" ")}> This is really working</p>
+        <button style={style} onClick={this.togglePesonDetails}>Toggle Person</button>
+        {/* <StyledButton alt={this.state.isPersonDetailsEnabled} onClick={this.togglePesonDetails}>Toggle Person</StyledButton> */}
+        {/* {
+          this.state.isPersonDetailsEnabled ?
+            <div>
+              <Person name={"santosh"} age="22"><b>Hobbies:Playing Cricket</b></Person>
+              <Person name="manohar" age="24" />
+
+            </div>
+            :
+            null
+        } */}
+        {personData}
+
+      </div>
+
+    );
+  }
+}
+
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,63 @@
+
+//-------using radium component-----------
+
+// import React from 'react'
+// import './Person.css'
+// import Radium from 'radium'
+// const Person=(props)=>{
+//     const style={
+//         '@media(min-width:500px)':{
+//             width:'450px',
+
+//         }
+//     }
+//     return(
+//         <div className="Person" style={style}>
+//         <h4 onClick={()=>props.deletePersonDetail(props.selectedindex)}>I am {props.name}. I have {props.age} years old</h4>
+//         <p>{props.children}</p>
+//         <input type="text" onChange={(e)=>props.nameChangHandler && props.nameChangHandler(e)} value={props.name}/>
+//         </div>
+//     )
+// }
+
+// export default Radium (Person)
+
+//-------using styledComonent---------
 import React from 'react'
 import './Person.css'
-const Person=(props)=>{
-    return(
-        <div className="Person">
-        <h4 onClick={()=>props.deletePersonDetail(props.selectedindex)}>I am {props.name}. I have {props.age} years old</h4>
-        <p>{props.children}</p>
-        <input type="text" onChange={(e)=>props.nameChangHandler && props.nameChangHandler(e)} value={props.name}/>
-        </div>
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+width:60%;
+border:1px solid #eee;
+margin: 16px auto;
+box-shadow: 0 2px 3px #ccc;
+padding:16px;
+text-align: center;
+
+'@media(min-width:500px)':{
+    width:'450px',
+    background-color:red
+ 
+}
+`
+
+const Person = (props) => {
+    const style = {
+        '@media(min-width:500px)': {
+            width: '450px',
+            backgroundColor: "red"
+
+        }
+    }
+    return (
+        // <div className="Person" style={style}>
+        <StyleDiv>
+            <h4 onClick={() => props.deletePersonDetail(props.selectedindex)}>I am {props.name}. I have {props.age} years old</h4>
+            <p>{props.children}</p>
+            <input type="text" onChange={(e) => props.nameChangHandler && props.nameChangHandler(e)} value={props.name} />
+            {/* </div> */}
+        </StyleDiv>
     )
 }
 
