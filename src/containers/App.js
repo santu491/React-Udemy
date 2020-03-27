4a925ccf952147cfe74113db07cf839fe03734a0
Santosh <santoshkumarm1996@gmail.com>
2020-03-27 11:33:38 +0530

    Added styled component to code

--- src/containers/App.js
+++ src/containers/App.js
@@ -176,7 +176,139 @@
 
 // export default App;
 
+//--------using Styled Componet------------------------
 
+// import React, { Component } from 'react';
+// import Person from '../components/Persons/Person/Person'
+// import './App.css';
+// // import Radium,{StyleRoot} from 'radium'
+// import styled from 'styled-components'
+// import Errorboundary from '../ErrorBoundary/ErrorBoundary'
+
+// const StyledButton = styled.button`
+// background-color:${props=>props.alt?'red':"green"},
+// color:white,  
+// font-size:17,
+// padding:16,
+// cursor:pointer,
+// &:hover {
+//   background-color:${props=>props.alt?"green":"lightGreen"},
+//   color:blue
+// `;
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       person: [
+//         { id: 0, name: "srinu", age: 25 },
+//         { id: 1, name: "Ram", age: 30 },
+//         { id: 2, name: "aravind", age: 25 }
+//       ],
+//       isPersonDetailsEnabled: false
+//     }
+
+//   }
+
+
+//   togglePesonDetails = () => {
+//     this.setState({
+//       isPersonDetailsEnabled: !this.state.isPersonDetailsEnabled
+
+//     })
+//   }
+
+//   nameChangHandler = (event, id) => {
+//     const personIndex = this.state.person.findIndex((p) => { return p.id === id })
+//     const person = { ...this.state.person[personIndex] }
+//     person.name = event.target.value;
+//     let persons = [...this.state.person];
+//     persons[personIndex] = Object.assign(persons[personIndex], person)
+//     this.setState({
+//       person: persons
+//     })
+//   }
+
+//   deletePersonDetail = (selectedndex) => {
+//     let persons = this.state.person
+//     persons.splice(selectedndex, 1)
+//     this.setState({
+//       person: persons
+//     })
+
+//   }
+
+//   render() {
+//     let personData = null
+//     const style = {
+//       backgroundColor: "green",
+//       color: "white",
+//       fontSize: 17,
+//       padding: 16,
+//       cursor: "pointer",
+//       ':hover': {
+//         backgroundColor: 'lightGreen',
+//         color: "blue"
+//       },
+//     }
+//     if (this.state.isPersonDetailsEnabled) {
+//       personData = this.state.person.map((person, index) => {
+//         return (
+//           <Errorboundary key={person.id}>
+//           <Person name={person.name} age={person.age}
+//             selectedindex={index}
+//             deletePersonDetail={(index) => this.deletePersonDetail(index)}
+//             nameChangHandler={(e) => this.nameChangHandler(e, person.id)}
+//             key={person.id}
+//           />
+//           </Errorboundary>
+//         )
+//       })
+//       style.backgroundColor = "red"
+//       style[':hover'] = {
+//         backgroundColor: 'samlon',
+//         color: "green"
+//       }
+//     }
+
+//     const classes = [];
+//     if (this.state.person.length <= 1) {
+//       classes.push("red")
+//     }
+//     else if (this.state.person.length >= 2) {
+//       classes.push("bold")
+//     }
+//     return (
+
+//       <div className="App">
+
+//         <h1>I am a React App</h1>
+//         <p className={classes.join(" ")}> This is really working</p>
+//         <button style={style} onClick={this.togglePesonDetails}>Toggle Person</button>
+//         {/* <StyledButton alt={this.state.isPersonDetailsEnabled} onClick={this.togglePesonDetails}>Toggle Person</StyledButton> */}
+//         {/* {
+//           this.state.isPersonDetailsEnabled ?
+//             <div>
+//               <Person name={"santosh"} age="22"><b>Hobbies:Playing Cricket</b></Person>
+//               <Person name="manohar" age="24" />
+
+//             </div>
+//             :
+//             null
+//         } */}
+//         {personData}
+
+//       </div>
+
+//     );
+//   }
+// }
+
+// export default App;
+
+
+//----------
 
 import React, { Component } from 'react';
 import Person from '../components/Persons/Person/Person'
